887f3e046d52d6b0962e7830912477955ad9b718
MilkyStaff <nikitosmotrov@gmail.com>
2024-12-09 21:45:26 +0700

    game entry point

---
+++ Assets/myProject/Scripts/Game/GameRoot/GameEntryPoint.cs
@@ -0,0 +1,39 @@
+﻿using Assets.myProject.Scripts.Game.GameRoot;
+using UnityEngine;
+
+namespace Assets.myProject.Scripts
+{
+    public class GameEntryPoint
+    {
+        private static GameEntryPoint _instance;
+
+        private CouroutinesHandler _couroutinesHandler;
+        private UIRootView _uiRoot;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void AutoStartGame()
+        {
+            _instance = new();
+
+            _instance.SetProjectSettings();
+            _instance.RunGame();
+        }
+
+        private GameEntryPoint()
+        {
+            _couroutinesHandler = new GameObject("[COROUTINES]").AddComponent<CouroutinesHandler>();
+            Object.DontDestroyOnLoad(_couroutinesHandler.gameObject);
+        }
+
+        private void RunGame()
+        {
+            
+        }
+
+        private void SetProjectSettings()
+        {
+            //Application.targetFrameRate = 60;
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+    }
+}
---
+++ Assets/myProject/Scripts/Game/GameRoot/UIRootView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.myProject.Scripts.Game.GameRoot
+{
+    public class UIRootView : MonoBehaviour
+    {
+        [SerializeField] private GameObject _loadingScreen;
+
+        private void Awake()
+        {
+            HideLoadingScreen();
+        }
+
+        public void ShowLoadingScreen()
+        {
+            _loadingScreen.SetActive(true);
+        }
+        public void HideLoadingScreen()
+        {
+            _loadingScreen.SetActive(false);
+        }
+    }
+}
